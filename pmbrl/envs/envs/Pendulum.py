1fe0942f6400d84eb43a1b0d443a0a32caad3928
Dala <2367758021@qq.com>
2023-03-16 14:49:29 +0800

    wu

---
+++ pmbrl/envs/envs/Pendulum.py
@@ -0,0 +1,25 @@
+import gym
+import numpy as np
+
+
+class Pendulum:
+    def __init__(self, render_mode = 'rgb_array'):
+        self.env = gym.make("Pendulum-v1",render_mode = render_mode)
+
+        self.action_space = self.env.action_space
+        self.observation_space = self.env.observation_space
+
+    def reset(self):
+        state, _ = self.env.reset()
+        return state
+
+    def step(self, action):
+        state, reward, term, trun, info = self.env.step(action)
+        done = np.bitwise_or(term, trun)
+        return state, reward, done, info
+
+    def render(self):
+        self.env.render()
+
+    def close(self):
+        self.env.close()
